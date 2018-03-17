89e73cec5983e26901b2844d8db6a52ed7ba7ded
Garrett Beatty <gbt@Garretts-MacBook-Pro.local>
2018-03-17 12:52:32 -0400

    trying new travis

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-openssl aes-256-cbc -K $encrypted_77bee7102036_key -iv $encrypted_77bee7102036_iv -in secrets.tar.enc -out secrets.tar -d
-tar xvf secrets.tar
+if [[ $TRAVIS_BRANCH == "master" && $TRAVIS_PULL_REQUEST == "false" ]]; then
+    openssl aes-256-cbc -K $encrypted_77bee7102036_key -iv $encrypted_77bee7102036_iv -in secrets.tar.enc -out secrets.tar -d
+    tar xvf secrets.tar
+fi
+
