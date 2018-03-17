c7a342f7e77525e6450c135b24b85861ed9aed31
Garrett Beatty <gbt@Garretts-MacBook-Pro.local>
2018-03-16 22:55:12 -0400

    updated travis

---
+++ scripts/after_success.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+if [[ $TRAVIS_BRANCH == "master" && $TRAVIS_PULL_REQUEST == "false" ]]; then
+#      fastlane beta
+fi
---
+++ scripts/before_install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+if [[ $TRAVIS_BRANCH == "master" && $TRAVIS_PULL_REQUEST == "false" ]]; then
+      openssl aes-256-cbc -K $encrypted_77bee7102036_key -iv $encrypted_77bee7102036_iv -in secrets.tar.enc -out secrets.tar -d
+      tar xvf secrets.tar
+fi
