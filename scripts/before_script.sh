6b02c1dcb26752b3a105a9b5be00b25fa419a76f
Garrett Beatty <gbt@Garretts-MacBook-Pro.local>
2018-03-17 12:40:23 -0400

    commented out emulator

--- scripts/before_script.sh
+++ scripts/before_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo no | android create avd --force -n test -t android-$ANDROID_EMU_API_LEVEL --abi google_apis/$ANDROID_ABI
-emulator -avd test -no-window &
-android-wait-for-emulator
-adb shell input keyevent 82 &
+#echo no | android create avd --force -n test -t android-$ANDROID_EMU_API_LEVEL --abi google_apis/$ANDROID_ABI
+#emulator -avd test -no-window &
+#android-wait-for-emulator
+#adb shell input keyevent 82 &
