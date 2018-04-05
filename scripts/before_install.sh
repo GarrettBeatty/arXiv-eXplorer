9ca31639e4fe76caf66baa4f33b0f60847adf2b9
Garrett Beatty <gbt@Garretts-MacBook-Pro.local>
2018-04-04 23:47:26 -0400

    updated secrets

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ $TRAVIS_BRANCH == "master" && $TRAVIS_PULL_REQUEST == "false" ]]; then
-    openssl aes-256-cbc -K $encrypted_77bee7102036_key -iv $encrypted_77bee7102036_iv -in secrets.tar.enc -out secrets.tar -d
-    tar xvf secrets.tar
+    openssl aes-256-cbc -K $encrypted_77bee7102036_key -iv $encrypted_77bee7102036_iv -in secrets.tar.gz.enc -out secrets.tar.gz -d
+    tar xzvf secrets.tar.gz
 fi
 
