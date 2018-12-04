cbd636a5f65a150dae7c996dc14eb4b25cd9d202
beattyg2 <beattyg2@tcnj.edu>
2018-12-04 16:02:37 -0500

    removed travis

--- scripts/after_success.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if [[ $TRAVIS_BRANCH == "master" && $TRAVIS_PULL_REQUEST == "false" ]]; then
-#    fastlane beta
-    :
-fi
--- scripts/before_script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#echo no | android create avd --force -n test -t android-$ANDROID_EMU_API_LEVEL --abi google_apis/$ANDROID_ABI
-#emulator -avd test -no-window &
-#android-wait-for-emulator
-#adb shell input keyevent 82 &
